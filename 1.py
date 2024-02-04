18d1cb2b21b1f6b0fddf0f6ee521c3c037850d0f
sachinsahu2002 <sachinkumarsahu2002@gmial.com>
2024-02-04 17:41:38 +0530

    This is Python Programming

---
+++ 1.py
@@ -0,0 +1,13 @@
+#Typecasting in python
+a = 1
+b = 2
+print(a+b)
+a = "1" #Explicit converion
+b ="2"
+print(a+b)
+print(int(a)+int(b))
+
+#Implicit typecasting
+c = 56.6
+d = 4
+print(c+d) 
---
+++ 2.py
@@ -0,0 +1,8 @@
+#Taking User input
+a = input("Enter your name: ")
+print("Hello! ", a)
+x = input("Enter first number: ")
+y = input("Enter first number: ")
+print(x+y, "Here the numbers are considered as strings")
+
+print(int(x)+int(y), "Here numbers are added")
