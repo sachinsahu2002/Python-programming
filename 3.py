9ecb018898758a53719b9fb86936cc9eeed1d80a
sachinsahu2002 <=>
2024-02-05 11:18:04 +0530

    Adding more files

---
+++ 3.py
@@ -0,0 +1,21 @@
+#Strings in python
+name = "Tom"
+friend = "Jerry"
+anotherFriend = "Buckky"
+# For writing Praragraphs in python
+para = '''Hi Tom
+this is a paragraph
+And you are reading this '''
+
+print("Hello," + name)
+print(para)
+
+print(name[0])
+print(name[1])
+print(name[2])
+# print(name[3]) This throws an error
+
+#Using For loop
+for character in friend:
+    print(character)
+
