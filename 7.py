cad669852b5de6314340699cb865b28f74aa5de6
sachinsahu2002 <=>
2024-02-06 21:16:04 +0530

    Conditional statements

---
+++ 7.py
@@ -0,0 +1,7 @@
+# Using conditional statements
+a = int(input("Enter your age: "))
+if (a>=18):
+    print("You are eligible for driving")
+else:
+    print("You are not eligible for driving")
+    
